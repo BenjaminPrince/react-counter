e82c5633376feea46f5f2606227eaf83227e2bad
BenjaminPrince <prince-benjamin@hotmail.com>
2022-06-28 14:23:50 +0200

    exercice todo

--- src/Greeting.js
+++ src/Greeting.js
@@ -2,10 +2,15 @@ import React from "react";
 
 export default class Greeting extends React.Component {
     render() {
+      
         return (
-            <h1>I greet you</h1>
+        //  
+         <h1>I greet you {this.props.count} times </h1>
+         
         );
     }
 }
+
+
 
 // TODO : Utiliser la prop "count" dans le message. Ex: avec "count" à 3, le message affichera "I greet you 3 times"
